1cca038947354ecda8c817d6c85c8c7706cda3ea
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2020-02-13 18:04:34 -0300

    Criando grid inicial da calculadora

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.resultado}></View>
+      <View style={styles.calculos}>
+        <View style={styles.numeros}>
+          <View style={styles.lines}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+          </View>
+          <View style={styles.lines}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+          </View>
+          <View style={styles.lines}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+          </View>
+          <View style={styles.lines}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+          </View>
+        </View>
+        <View style={styles.simbolos}>
+          <View style={styles.buttons}></View>
+          <View style={styles.buttons}></View>
+          <View style={styles.buttons}></View>
+          <View style={styles.buttons}></View>
+          <View style={styles.buttons}></View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +43,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    backgroundColor: 'black'
   },
+  resultado: {
+    flex: 1, 
+    backgroundColor: "#fff"
+  },
+  calculos: {
+    flex: 3, 
+    flexDirection: "row"
+  },
+  numeros: {
+    flex: 3,
+  },
+  simbolos:{
+    flex: 1,
+    margin: 1, 
+    alignItems: 'stretch', 
+    justifyContent: "space-around"
+  },
+  buttons:{
+    flex: 1,
+    margin: 1, 
+    backgroundColor: "#282525",
+  }, 
+  lines: {
+    flex: 1, 
+    margin: 1, 
+    flexDirection: "row"
+  }
+ 
 });
